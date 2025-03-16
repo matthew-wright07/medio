c003ca4facb9c718e6a97495d306d9649471f555
Matthew <matthewjwright07@gmail.com>
2025-03-16 16:22:55 -0500

    Got rid of some stuff

--- app/crunchyroll/page.js
+++
@@ -1,8 +0,0 @@
-export default function Crunchyroll() {
-    return (
-        <iframe 
-            src="https://www.crunchyroll.com/" 
-            style={{ height: '100vh', width: '100vw' }} 
-        />
-    );
-}
